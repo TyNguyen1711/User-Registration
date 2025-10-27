085342f2dfbfc6a1cc27e73c283a88997aae620e
22120419 <nguyenvanty147258@gmail.com>
2025-10-27 13:11:56 +0700

    updated

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,16 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://user-registration-frontend1.vercel.app',
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3000);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -20,7 +12,5 @@ async function bootstrap() {
       transform: true,
     }),
   );
-
-  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
